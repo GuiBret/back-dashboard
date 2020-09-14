baca4f04b4195b34e8da2a482bc2c0f25a31ebb7
GuiBret <guillaume.bretzner@gmail.com>
2020-09-14 22:50:17 +0200

    Add schema

--- backend/models/todo.schema.ts
+++ backend/models/todo.schema.ts
@@ -1,13 +1,20 @@
 import { Document } from 'mongoose';
 import { Prop, SchemaFactory, Schema } from '@nestjs/mongoose';
-
+import * as mongoose from 'mongoose';
 @Schema()
 export class TodoMongoose extends Document {
+
+    @Prop()
+    _id: mongoose.Types.ObjectId;
+    
     @Prop({required: true})
     title: string;
     
     @Prop({required: true})
     content: string;
+
+    @Prop({default: false})
+    status: boolean;
 }
 
 
