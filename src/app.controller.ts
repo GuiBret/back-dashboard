a81c1930f7b8a689738d4b0d4a22d7ed0bc6a0e3
GuiBret <guillaume.bretzner@gmail.com>
2020-09-14 22:50:06 +0200

    Add save all + edit todo features

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Req, Request, ParamData, Param } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { TodoMongoose } from 'backend/models/todo.schema';
+import * as mongoose from 'mongoose';
 
 @Controller()
 export class AppController {
@@ -19,5 +20,25 @@ export class AppController {
   @Get('todos')
   getAllTodos() {
     return this.appService.getTodos();
+  }
+
+  @Post('todos') 
+  saveTodos(@Req() request){
+    const form: any = request.body;
+    const todos = form.todos;
+
+    return this.appService.saveTodos(todos);
+  }
+
+  @Post('todos/:idtodo/edit')
+  editTodo(@Req() request: Request, @Param() params) {
+
+    
+    const form : any = request.body;
+    const idTodo: string = params.idtodo;
+    const todoData : TodoMongoose = form.todo;
+    return this.appService.editTodo(idTodo, todoData);
+    
+    // return this.appService.saveTodos(todoData);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,9 @@ import { EasyconfigService } from 'nestjs-easyconfig';
 import { Connection, Model } from 'mongoose';
 import { TodoMongoose } from 'backend/models/todo.schema';
 import { InjectModel } from '@nestjs/mongoose';
+import { Observable, observable } from 'rxjs';
+import { stringify } from 'querystring';
+import * as mongoose from 'mongoose';
 
 @Injectable()
 export class AppService {
@@ -10,6 +13,7 @@ export class AppService {
   
 
   constructor(@InjectModel(TodoMongoose.name) private todoModel: Model<TodoMongoose>) {
+    
     
   }
   
@@ -26,5 +30,48 @@ export class AppService {
   addTodo(todoToAdd: TodoMongoose) {
     this.todoModel.create(todoToAdd);
   }
+
+  saveTodos(todoListToAdd: Array<TodoMongoose>): Observable<any>{
+
+    return new Observable((observer) => {
+      
+        this.todoModel.create(todoListToAdd).then((response) => {
+          
+          observer.next({status: 'OK', list: response});
+          observer.complete();
+          return response;
+        }).catch((err) => {
+          observer.error(err);
+          observer.complete();
+        })
+    });
+
+    
+    
+  }
+
+  editTodo(idTodo: string, todo: TodoMongoose) {
+
+    return new Promise(async (resolve, reject) => {
+
+      todo._id = mongoose.Types.ObjectId(idTodo);
+      
+      
+    // const todoToEdit = await this.todoModel.findById(mongoose.Types.ObjectId(idTodo)).exec();
+      const todoToEdit = await this.todoModel.findOne(mongoose.Types.ObjectId(idTodo)).exec();
+      
+      if(!todoToEdit) {
+        resolve({status: 'KO', error: 'NOT_FOUND'});
+      } else {
+        todoToEdit.updateOne(todo).exec();
+        resolve({status: 'OK'});
+      }
+      
+    });
+
+  } 
+  
 }
+
+
  
